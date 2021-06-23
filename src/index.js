e7a341028a0b9bf2f810ba98b030b8845a18f61d
Marcos André <macam@cin.ufpe.br>
2021-06-22 22:35:36 -0300

    fix(main): fixing github pages support

--- src/index.js
+++ src/index.js
@@ -2,15 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import BrowserRouter from 'react-router-dom/BrowserRouter'
 
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-  
-   </BrowserRouter>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
